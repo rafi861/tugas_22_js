41f0d7f27c7b1636dd64dba835831f46a81b5405
rafi861 <muhammadrafi2545@gmail.com>
2023-03-11 06:50:19 +0700

    tugas 22 js

---
+++ tugas22.js
@@ -0,0 +1,10 @@
+function tugas() {
+  var data = ["item : saya index ke 0"
+  ,"item : ingin index ke 1","item : belajar index ke 2"
+  ,"item : bersama index ke 3"]
+  data.forEach((item, string, i) => {
+    console.log(item);
+  });
+
+}
+tugas()
